b6965598a2bfbbbb58de7e9fcee7c6f7a4fa61ca
K31Sonawane <sonawanekisho31@gmail.com>
2026-02-22 03:00:14 +0530

    dhange

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,24 @@
 
+// import { Pool } from "pg";
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: 5432,
+//   ssl: false
+// });
+
+// export default pool;
+
 import { Pool } from "pg";
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 5432,
-  ssl: false
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // needed if remote DB requires SSL
+  },
 });
 
 export default pool;
-
 
