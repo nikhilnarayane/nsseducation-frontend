28ca29553949a9197ae624acbc9f992c5beeefda
K31Sonawane <sonawanekisho31@gmail.com>
2026-03-16 17:59:10 +0530

    chnages done

--- src/app/api/test-db/route.ts
+++ src/app/api/test-db/route.ts
@@ -1,15 +1,19 @@
-import pool from "@/lib/db";
+import pool from "lib/db";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
     const result = await pool.query("SELECT NOW()");
-    console.log("DB Connected:", result.rows);
-
-    return Response.json({ success: true });
-
-  } catch (error) {
-    console.error("DB Error:", error);
-
-    return Response.json({ success: false });
+    return NextResponse.json({
+      success: true,
+      time: result.rows[0],
+    });
+    
+  } catch (error: any) {
+    console.error("DB CONNECTION ERROR:", error);
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 500 }
+    );
   }
 }
