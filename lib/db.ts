82acce1298fa6377fc0ae7033b942ead1bdbeaa9
K31Sonawane <sonawanekisho31@gmail.com>
2026-03-16 16:19:01 +0530

    chnages done

--- lib/db.ts
+++ lib/db.ts
@@ -5,8 +5,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  // port: 5432,
-  ssl: true
+  ssl: false
 });
 export default pool;
 
