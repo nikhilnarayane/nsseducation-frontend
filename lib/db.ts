ca71322963ede6154e9d5dea10593eed45bf121e
nikhilnarayane <43135206+nikhilnarayane@users.noreply.github.com>
2026-03-16 17:12:13 +0530

    Enable PostgreSQL Pool connection in db.ts
    Uncommented and configured the Pool connection for PostgreSQL.

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,13 @@
 
-// import { Pool } from "pg";
-// const pool = new Pool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   ssl: true
-// });
-// export default pool;
+import { Pool } from "pg";
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: true
+});
+export default pool;
 
 // import { Pool } from "pg";
 
@@ -20,19 +20,7 @@
 
 // export default pool;
 
-import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.DB_USER!,
-  process.env.DB_PASSWORD!,
-  {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: "postgres",
-    logging: false
-  }
-);
 
 export default sequelize;
 
