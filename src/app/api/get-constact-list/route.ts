9614c6e4f624c717f85eb5ee251300994085b010
nikhilnarayane <nikhil.casio@gmail.com>
2026-03-19 15:37:44 +0530

    changes are done

--- src/app/api/get-constact-list/route.ts
+++ src/app/api/get-constact-list/route.ts
@@ -1,1 +1,15 @@
 
+import pool from "@/lib/db";
+import { NextResponse } from "next/server";
+export async function GET() {
+  try {
+    const result = await pool.query("SELECT * FROM contact ORDER BY id ASC");
+    return NextResponse.json(result, { status: 200 });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("DB Error:", error.message);
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json({ error: "Unknown DB error" }, { status: 500 });
+  }
+}
