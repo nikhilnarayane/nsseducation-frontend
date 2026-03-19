0394e74be036dec6a4e90356088d34618f603ba6
nikhilnarayane <nikhil.casio@gmail.com>
2026-03-19 13:22:34 +0530

    host level changes done

--- lib/test-db-connection.ts
+++ lib/test-db-connection.ts
@@ -4,7 +4,7 @@ import pool from "./db"; // make sure path is correct
   try {
     debugger
     const res = await pool.query("SELECT NOW()");
-    console.log("DB connected successfully:", res.rows[0]);
+    console.log("DB connected successfully:", res);
     process.exit(0); // exit script
   } catch (err) {
     console.error("DB connection error:", err);
